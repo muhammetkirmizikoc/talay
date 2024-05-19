ac1b8216e5b5dfed2af8f6a84f7f2acdfcd1a10e
Muhammet <129357623+muhammetkirmizikoc@users.noreply.github.com>
2024-05-19 16:24:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -74,3 +74,22 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+
+// FAQ Script 
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
